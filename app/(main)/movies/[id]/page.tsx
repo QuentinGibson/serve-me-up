761cad5608409cf0b9a00082736e6ee155a974bd
Quentin Gibson <quentingibson94@gmail.com>
2024-10-15 22:15:55 +0000

    add use client to details page

--- app/(main)/movies/[id]/page.tsx
+++ app/(main)/movies/[id]/page.tsx
@@ -1,8 +1,9 @@
+"use client"
 import MovieDetailsPage from "@/components/app-movies-details-page";
 import { useParams } from "next/navigation";
 
 
-export default async function Page() {
+export default function Page() {
   const params = useParams();
   const id = params.id as string
   return <MovieDetailsPage id={id} />;
